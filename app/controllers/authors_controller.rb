63f70b6bf89abf5a41960676afbd66e4cbd31f3e
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-27 11:09:59 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,10 +8,29 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
+    @author = Author.new(author_params)
 
-    redirect_to author_path(@author)
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    @author = Author.find(params[:id])
+    @author = Author.update(author_params)
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
+  end
+
 
   private
 
