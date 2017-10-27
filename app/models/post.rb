618e2ccdcb425925d9f22676e5ae7b00704a6c3e
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-27 10:29:04 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 100 }
-  validates :phone_number, length: { is: 10 }
+  alidates :category, inclusion: { in: %w(Fiction Non-Fiction)}
 end
