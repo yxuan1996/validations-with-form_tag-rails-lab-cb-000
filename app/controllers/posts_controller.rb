397f36d720e41593d8ea5be260e8baee60e2ea6c
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-27 10:34:17 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,12 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+  end
+
   def edit
     @post = Post.find(params[:id])
   end
